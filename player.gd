113735f300ba072428afca4ce3738e356ffa42ae
Harrison Lee <hrl5000@gmail.com>
2025-06-18 23:38:42 -0400

    Updates player.
    - Adds climbing and descent acceleration as exported variables
    - Adds auto descent boolean to determine whether to use custom descent
      variable
    - Formats code.

--- player.gd
+++ player.gd
@@ -1,40 +1,49 @@
 extends CharacterBody2D
 
-const CLIMB_VELOCITY = -3000.0
-const DESCEND_VELOCITY = 2200.0
+## Climbing acceleration in px/s.
+@export var climb_acceleration: float = 3000.0
+## Use descent acceleration as function of climb accel minus gravity. Note,
+## descent custom descent acceleration is ignore if this is true.
+@export var use_auto_descent_acceleration: bool = true
+## Descent acceleration in px/s. Ignored if use auto descent acceleration is
+## true.
+@export var descent_acceleration: float = 0.0
 
 enum FlyingState { IDLE, CLIMBING, DESCENDING }
-var currentFlyingState = FlyingState.IDLE
 
-@onready var collision_body: CollisionShape2D = $CollisionShape2D
 var screen_size: Vector2
+var current_flying_state = FlyingState.IDLE
+@onready var collision_body: CollisionShape2D = $CollisionShape2D
+
 
 func _draw() -> void:
 	draw_circle(Vector2.ZERO, 50.0, Color.RED)
 	screen_size = get_viewport_rect().size
 
 
-
 func get_input() -> void:
-	currentFlyingState = FlyingState.IDLE
+	current_flying_state = FlyingState.IDLE
 	if Input.is_action_pressed("ui_climb"):
-		currentFlyingState = FlyingState.CLIMBING
+		current_flying_state = FlyingState.CLIMBING
 	elif Input.is_action_pressed("ui_descend"):
-		currentFlyingState = FlyingState.DESCENDING
-
-	print(currentFlyingState)
+		current_flying_state = FlyingState.DESCENDING
 
 
 func _physics_process(delta: float) -> void:
 	get_input();
-	var gravity = get_gravity();
 
-	if currentFlyingState == FlyingState.CLIMBING:
-		velocity.y += CLIMB_VELOCITY * delta
-	elif currentFlyingState == FlyingState.DESCENDING:
-		velocity.y += (DESCEND_VELOCITY + gravity.y) * delta
-	else:
-		velocity.y += gravity.y * delta
+	var gravity = get_gravity();
+	var descent_accel = descent_acceleration
+	if use_auto_descent_acceleration:
+		descent_accel = climb_acceleration - gravity.y
+
+	match current_flying_state:
+		FlyingState.CLIMBING:
+			velocity.y += -climb_acceleration * delta
+		FlyingState.DESCENDING:
+			velocity.y += (descent_accel + gravity.y) * delta
+		FlyingState.IDLE:
+			velocity.y += gravity.y * delta
 
 	velocity.x = 0
 
