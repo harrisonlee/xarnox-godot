8cd8f76b31cbb0db54747284e1c6f7ebd24c0e65
Harrison Lee <hrl5000@gmail.com>
2025-07-02 15:34:57 -0400

    Adds commented sections and removes semi-colons.

--- hud.gd
+++ hud.gd
@@ -1,12 +1,22 @@
 extends CanvasLayer
 
+#-------------------------------------------------------------------------------
+# On-ready Nodes
+#-------------------------------------------------------------------------------
 @onready var player_health_bar = $PlayerHealthBar
 
+
+#-------------------------------------------------------------------------------
+# Lifecyle Methods
+#-------------------------------------------------------------------------------
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player_health_bar.init_health(100.0)
 
 
+#-------------------------------------------------------------------------------
+# Public Methods
+#-------------------------------------------------------------------------------
 func set_player_health(health: float) -> void:
 	player_health_bar.health = health
 
@@ -15,7 +25,3 @@ func adjust_player_health(amount: float) -> void:
 	var current_health = player_health_bar.health
 	player_health_bar.health = current_health + amount
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
--- main.gd
+++ main.gd
@@ -1,29 +1,46 @@
 extends Node
 
-var current_viewport_rect: Rect2 = Rect2()
-var tunnel_y_origin: float = 0.0
-
+#-------------------------------------------------------------------------------
+# On-ready Nodes
+#-------------------------------------------------------------------------------
 @onready var hud = $HUD
 @onready var player = $Player
 @onready var player_camera = $PlayerCamera
 @onready var tunnel_generator = $TunnelGenerator
 
+
+#-------------------------------------------------------------------------------
+# Private Variables
+#-------------------------------------------------------------------------------
+var _current_viewport_rect: Rect2 = Rect2()
+var _tunnel_y_origin: float = 0.0
+
+
+#-------------------------------------------------------------------------------
+# Lifecycle Methods
+#-------------------------------------------------------------------------------
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.position = $PlayerStartingPosition.position
 	player_camera.player_follow_x = $PlayerStartingPosition.position.x
 	player_camera.move_to_player()
 
-	current_viewport_rect = get_viewport().get_visible_rect()
-	tunnel_y_origin = current_viewport_rect.get_center().y
-	tunnel_generator.generate_tunnel(current_viewport_rect, tunnel_y_origin)
+	_current_viewport_rect = get_viewport().get_visible_rect()
+	_tunnel_y_origin = _current_viewport_rect.get_center().y
+	tunnel_generator.generate_tunnel(_current_viewport_rect, _tunnel_y_origin)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	tunnel_generator.generate_tunnel(player_camera.get_visible_rect(), tunnel_y_origin)
+	tunnel_generator.generate_tunnel(
+		player_camera.get_visible_rect(),
+		_tunnel_y_origin
+	)
 
 
+#-------------------------------------------------------------------------------
+# Signal Callbacks
+#-------------------------------------------------------------------------------
 func _on_player_hit() -> void:
 	player_camera.shake()
 	hud.adjust_player_health(-10)
